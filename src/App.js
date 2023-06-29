265fd34c283ec2b534f38ee579c62881a885c864
devmarkos <128612734+devmarkos@users.noreply.github.com>
2023-06-29 16:48:04 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import {database} from "./firebase-config"
-import {collection, getDocs} from "firebase/firestore"
+import {collection, getDocs, addDoc, updateDoc, deleteDoc, doc} from "firebase/firestore"
 
 function App() {
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
+
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(database, "users");
 
-  // ez akkor fut le ha betölt az oldal
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {name : newName, age : Number(newAge)});
+
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(database, "users", id)
+    const newFields = {age: age + 1}
+    await updateDoc(userDoc, newFields)
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(database, "users", id)
+    await deleteDoc(userDoc);
+
+  };
+
+  // ez akkor fut le amikor betölt az oldal
   useEffect(() => {
 
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data, id: doc.id })));
-      //console.log(data)
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
       console.log(users);
     };
 
@@ -23,12 +42,18 @@ function App() {
 
   return (
     <div className="App">
+      <input placeholder='Name..' onChange={(event) => {setNewName(event.target.value)}}/>
+      <input type='number' placeholder='Age..' onChange={(event) => {setNewAge(event.target.value)}}/>
+
+      <button onClick={createUser}>Create User</button>
+
       { users.map((user) => {
         return (
           <div>
-            <h1>{user.id}</h1>
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
+            <button onClick={() => {updateUser(user.id, user.age)}}>Incrase age</button>
+            <button onClick={() => {deleteUser(user.id)}}>Delete user</button>
           </div>
         );
       })}
